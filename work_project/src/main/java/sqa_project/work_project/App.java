66ea3d28d3794c6e3b702de9adfa35d4828afe6a
maratvl1 <maratvl@gmail.com>
2015-01-26 21:51:10 -0800

    test

---
+++ work_project/src/main/java/sqa_project/work_project/App.java
@@ -0,0 +1,16 @@
+package sqa_project.work_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String str = "Marat";
+        System.out.println(str);
+        
+    }
+}
